015fc7edb264a1a743a1e099ad42fe4ff3779f9d
y.mitina <mitina.yv.48@gmail.com>
2024-10-09 21:09:54 +0300

    Добавлен вывод строки на русском языке

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -3,5 +3,6 @@ package ru.otus.java.basic.homeworks;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Привет мир!!!");
     }
 }
