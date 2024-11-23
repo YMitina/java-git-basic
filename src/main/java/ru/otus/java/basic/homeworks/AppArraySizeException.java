62f29e90bb97dd63a4dea54248bdce748c5c8f61
y.mitina <mitina.yv.48@gmail.com>
2024-11-23 14:08:24 +0300

    ДЗ_8

---
+++ src/main/java/ru/otus/java/basic/homeworks/AppArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks;
+
+public class AppArraySizeException extends Throwable {
+    public AppArraySizeException(String s) {
+        System.out.println(s);
+
+    }
+}
