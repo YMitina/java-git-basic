a95ef39afe43c3028cd85b94a8f52b0b3779a705
y.mitina <mitina.yv.48@gmail.com>
2024-12-08 21:25:09 +0300

    ДЗ_12 выполнено

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,8 +1,73 @@
 package ru.otus.java.basic.homeworks;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!!!");
+
+
+        File file = new File(".");
+        System.out.println("file.listFiles() = " + Arrays.toString(file.listFiles()));
+
+        System.out.println();
+
+        System.out.println("Доступные текстовые файлы с расширением txt:");
+        for (File files : file.listFiles()) {
+            if (files.getName().toString().indexOf(".txt") != -1) {
+                System.out.println(files.getName().toString());
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя файла из доступных выше, для просмотра его содержимого:");
+        String nameFile = scanner.nextLine();
+
+        System.out.println();
+
+        System.out.println("Содержимое файла " + nameFile + ":");
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(nameFile))) {
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        System.out.println("Введите строку для записи в файл:");
+        String newStringFile = scanner.nextLine();
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(nameFile, true))) {
+            byte[] buffer = newStringFile.getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                out.write(buffer[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        System.out.println("Содержимое файла " + nameFile.toString() + " после добавления в конец файла:");
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(nameFile))) {
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
     }
 }
