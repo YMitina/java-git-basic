d5da783dee6e697ddb9399c25905b9c5f62a9a7f
y.mitina <mitina.yv.48@gmail.com>
2024-12-12 22:16:20 +0300

    ДЗ_13 выполнено

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,8 +1,8 @@
 package ru.otus.java.basic.homeworks;
 
+
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!!!");
     }
 }
