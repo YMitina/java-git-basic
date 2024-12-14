d197069616cdafcd01db0c819bb7a68dd9c0ab70
y.mitina <mitina.yv.48@gmail.com>
2024-12-15 01:00:44 +0300

    ДЗ_14 Выполнено

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,8 +1,38 @@
 package ru.otus.java.basic.homeworks;
 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!!!");
+    public static void main(String[] args) throws InterruptedException {
+        double arr[] = new double[100_000_000];
+        Array array = new Array();
+
+        Thread t1 = new Thread(() -> {
+            array.filling(arr, 0, 25_000_000, 1);
+        });
+
+        Thread t2 = new Thread(() -> {
+            array.filling(arr, 25_000_000, 50_000_000, 2);
+        });
+
+        Thread t3 = new Thread(() -> {
+            array.filling(arr, 50_000_000, 75_000_000, 3);
+        });
+
+        Thread t4 = new Thread(() -> {
+            array.filling(arr, 75_000_000, 100_000_000, 4);
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
     }
+
 }
+
+
---
+++ src/main/java/ru/otus/java/basic/homeworks/Counter.java
@@ -0,0 +1,11 @@
+package ru.otus.java.basic.homeworks;
+
+public class Counter {
+    public int value;
+    public void Counter(){
+        value =0;
+    }
+    public void increment(){
+        value++;
+    }
+}
