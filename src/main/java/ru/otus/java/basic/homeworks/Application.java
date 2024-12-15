c97d5f49f07edc583d1b10d802f16e31390fa229
y.mitina <mitina.yv.48@gmail.com>
2024-12-15 18:32:28 +0300

    ДЗ_14 добавлены join()

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,15 +1,10 @@
 package ru.otus.java.basic.homeworks;
 
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         double arr[] = new double[100_000_000];
         Array array = new Array();
-
+        long time = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
             array.filling(arr, 0, 25_000_000, 1);
         });
@@ -30,6 +25,13 @@ public class Application {
         t2.start();
         t3.start();
         t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        System.out.println("Время выполнения в 4-х потоках: " + (System.currentTimeMillis() - time));
 
     }
 
--- src/main/java/ru/otus/java/basic/homeworks/Array.java
+++ src/main/java/ru/otus/java/basic/homeworks/Array.java
@@ -4,26 +4,16 @@ import java.time.LocalTime;
 
 public class Array {
     public void filling(double arr[], int startIndex, int endIndex, int numberThread) {
-        if (numberThread != 0) {
-            System.out.println("Время начала выполнения " + numberThread + " потока:" + LocalTime.now());
-        } else {
-            System.out.println("Время начала выполнения в одном потоке:" + LocalTime.now());
-        }
-
         for (int i = startIndex; i < endIndex; i++) {
             arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-        }
-
-        if (numberThread != 0) {
-            System.out.println("Время окончания выполнения " + numberThread + " потока:" + LocalTime.now());
-        } else {
-            System.out.println("Время окончания выполнения в одном потоке:" + LocalTime.now());
         }
     }
 
     public static void main(String[] args) {
+        long time = System.currentTimeMillis();
         Array array = new Array();
         double arr[] = new double[100_000_000];
         array.filling(arr, 0, 100_000_000, 0);
+        System.out.println("Время выполнения в одном потоке: " + (System.currentTimeMillis() - time));
     }
 }
