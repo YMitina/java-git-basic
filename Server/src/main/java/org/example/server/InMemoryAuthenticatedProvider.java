8554ebaa9eedb0d2c821493491a8ef3c8d017f0b
y.mitina <mitina.yv.48@gmail.com>
2025-01-14 16:48:11 +0300

    ДЗ_18 работа над ошибками

--- Server/src/main/java/org/example/server/InMemoryAuthenticatedProvider.java
+++ Server/src/main/java/org/example/server/InMemoryAuthenticatedProvider.java
@@ -12,17 +12,26 @@ public class InMemoryAuthenticatedProvider implements AuthenticatedProvider {
 
     //
     private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERS_QUERY = "select uc.username from public.users_chat uc where uc.login = ? and uc.\"password\" = ?";
+    private static final String ROLE_ID_QUERY = "select rc.role_id from public.roles_chat rc where rc.title = ? ";
 
-    private static final String USERS_QUERY = "select max(uc.username) as username from public.users_chat uc where uc.login = ? and uc.\"password\" = ?";
-    private static final String USERS_ADMIN ="""
-                    select count(*) from public.users_chat uc where uc.login = ? and uc.\"role\" = 'ADMIN'
+    private static final String LOGIN_ADMIN ="""
+                    select count(*) from public.users_chat uc
+					join public.users_roles_chat urc on urc.login = uc.login
+					join public.roles_chat rc on urc.role_id = rc.role_id and rc.title = 'ADMIN'
+					where uc.login = ? 
                     """;
     private static final String USERNAME_ADMIN = """
-                    select count(*) from public.users_chat uc where uc.username = ? and uc.\"role\" = 'ADMIN'
+					select count(*) from public.users_chat uc
+					join public.users_roles_chat urc on urc.login = uc.login
+					join public.roles_chat rc on urc.role_id = rc.role_id and rc.title = 'ADMIN'
+					where uc.username = ? 
                     """;
     private static final String LOGIN_EXIST = "select count(*) as username from public.users_chat uc where uc.login = ? ";
     private static final String USERNAME_EXIST = "select count(*) as username from public.users_chat uc where uc.username = ? ";
-    private static final String INSERT_USER = "insert into  public.users_chat (login, \"password\", username, \"role\") values (?, ?, ?, ?)";
+    private static final String INSERT_USER = "insert into  public.users_chat (login, \"password\", username) values (?, ?, ?)";
+    private static final String INSERT_USER_ROLE = "insert into  public.users_roles_chat  (login, role_id) values (?, ?)";
+
 
     public InMemoryAuthenticatedProvider(Server server) {
         this.server = server;
@@ -32,7 +41,6 @@ public class InMemoryAuthenticatedProvider implements AuthenticatedProvider {
     public void initialize() {
         try {
             this.connection = DriverManager.getConnection(DATABASE_URL, "postgres", "admin");
-            this.statement = this.connection.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -63,7 +71,7 @@ public class InMemoryAuthenticatedProvider implements AuthenticatedProvider {
     }
 
     private boolean getIsAdmin(String login) throws SQLException {
-        PreparedStatement preparedStatement = this.connection.prepareStatement(USERS_ADMIN);
+        PreparedStatement preparedStatement = this.connection.prepareStatement(LOGIN_ADMIN);
         preparedStatement.setString(1, login);
 
         try (ResultSet resultSet = preparedStatement.executeQuery()) {
@@ -156,18 +164,34 @@ public class InMemoryAuthenticatedProvider implements AuthenticatedProvider {
         }
 
         try {
-            connection.setAutoCommit(false);
-            // создаём пользователя
-            PreparedStatement ps = connection.prepareStatement(INSERT_USER);
-            ps.setString(1, login);
-            ps.setString(2, password);
-            ps.setString(3, username);
-            ps.setString(4, "USER");
-            ps.executeUpdate();
 
-            connection.commit();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, username);
+            preparedStatement.executeUpdate();
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement(ROLE_ID_QUERY);
+            preparedStatement2.setString(1, "USER");
+
+            int id_role = 0;
+            try (ResultSet resultSet = preparedStatement2.executeQuery()) {
+                while (resultSet.next()) {
+                    id_role = resultSet.getInt(1);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
+            PreparedStatement preparedStatement3 = connection.prepareStatement(INSERT_USER_ROLE);
+            preparedStatement3.setString(1, login);
+            preparedStatement3.setInt(2, id_role);
+            preparedStatement3.executeUpdate();
+
+        } catch (
+                SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
