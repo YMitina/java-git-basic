a5af1659eb5987fcf3ffa3006d524440eacd85eb
y.mitina <mitina.yv.48@gmail.com>
2025-01-19 15:39:34 +0300

    ДЗ_20 выполнено

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,8 +1,38 @@
 package ru.otus.java.basic.homeworks;
 
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!!!");
+        String filename= "2.txt";
+        String text = "Hello World! Привет Мир! Прекрасный Мир!";
+        String substrig = "Мир";
+        createFile(filename, text );
+        System.out.println("Количество подстроки '"  +  substrig +"' в файле '" + filename +   "' равно: " + findSubstrigToFile(filename, substrig));
+    }
+    public static void createFile(String filename, String text){
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(filename))) {
+            String strForWrite = text;;
+            bufferedOutputStream.write(strForWrite.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+        public static int findSubstrigToFile(String filename, String substrig){
+            try (FileInputStream fileInputStream = new FileInputStream(filename)) {
+                byte[] buffer = fileInputStream.readAllBytes();
+                String data = new String(buffer, StandardCharsets.UTF_8);
+                    int count = 0;
+                    for (int idx = 0; (idx =data.indexOf(substrig, idx)) != -1; idx += substrig.length()) {
+                        count++;
+                    }
+                    return count;
+                } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
